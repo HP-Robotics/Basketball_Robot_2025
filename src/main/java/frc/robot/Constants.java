85577cc1ade4045b6e4d12943c05e0e39d1a3296
einstein2823 <teamautomatons2823@gmail.com>
2025-11-20 17:10:12 -0600

    add shooter

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -5,20 +5,33 @@
 package frc.robot;
 
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants
+ * should be declared
  * globally (i.e. public static). Do not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
+    public static final int kShooterButtonId = 1;
   }
 
   public static class ShooterConstants {
-    public static final double shooterSpeed = -0.75;
+    public static final int shooterLeaderMotorId = 41;
+    public static final int shooterFollowerMotorId = 42;
+    public static final double shooterAllowedError = 5;
+    public static final double shooterSpeed = -50;
+    public static final double shooterkP = 0.035;
+    public static final double shooterkI = 0.01;
+    public static final double shooterkD = 0;
+    public static final double shooterkS = 0.253906;
+    public static final double shooterkV = 0.0091;
   }
 
 }
--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -4,29 +4,48 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.Follower;
+import com.ctre.phoenix6.controls.VelocityDutyCycle;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ShooterConstants;
 
 public class ShooterSubsystem extends SubsystemBase {
   /** Creates a new ShooterSubsystem. */
   // this is not the right device ID.
-  public TalonFX m_shooterMotor1 = new TalonFX(41);
-  public TalonFX m_shooterMotor2 = new TalonFX(42);// TODO
+  public TalonFX m_shooterLeaderMotor = new TalonFX(ShooterConstants.shooterLeaderMotorId);
+  public TalonFX m_shooterFollowerMotor = new TalonFX(ShooterConstants.shooterFollowerMotorId);
+  private boolean m_isRunning = false;
 
   public ShooterSubsystem() {
+    m_shooterLeaderMotor.getConfigurator().apply(new TalonFXConfiguration());
+    Slot0Configs slot0Configs = new Slot0Configs();
+    slot0Configs.kP = ShooterConstants.shooterkP;
+    slot0Configs.kI = ShooterConstants.shooterkI;
+    slot0Configs.kD = ShooterConstants.shooterkD;
+    slot0Configs.kS = ShooterConstants.shooterkS;
+    slot0Configs.kV = ShooterConstants.shooterkV;
+    m_shooterLeaderMotor.getConfigurator().apply(slot0Configs);
+    m_shooterLeaderMotor.setNeutralMode(NeutralModeValue.Coast);
+
+    m_shooterFollowerMotor.getConfigurator().apply(new TalonFXConfiguration());
+    m_shooterFollowerMotor.setNeutralMode(NeutralModeValue.Coast);
+    m_shooterFollowerMotor.setControl(new Follower(ShooterConstants.shooterLeaderMotorId, false));
   }
 
   public void runShooter() {
-    m_shooterMotor1.set(ShooterConstants.shooterSpeed);
-    m_shooterMotor2.set(ShooterConstants.shooterSpeed);
+    m_shooterLeaderMotor.setControl(new VelocityDutyCycle(ShooterConstants.shooterSpeed));
+    m_isRunning = true;
+
   }
 
   public void stopShooter() {
-    m_shooterMotor1.set(0);
-    m_shooterMotor2.set(0);
+    m_shooterLeaderMotor.set(0);
+    m_isRunning = false;
   }
 
   @Override
@@ -35,11 +54,9 @@ public class ShooterSubsystem extends SubsystemBase {
   }
 
   public boolean atSpeed() {
-    if (this.m_shooterMotor1.getVelocity().getValueAsDouble() / 512.0 <= ShooterConstants.shooterSpeed
-        && this.m_shooterMotor2.getVelocity().getValueAsDouble() / 512.0 <= ShooterConstants.shooterSpeed) {
-      return true;
-    } else {
-      return false;
-    }
+    // TODO test this later
+    return Math
+        .abs(this.m_shooterLeaderMotor.getClosedLoopError().getValueAsDouble()) <= ShooterConstants.shooterAllowedError
+        && m_isRunning;
   }
 }
